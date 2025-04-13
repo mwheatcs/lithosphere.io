8473551385876ccda113df31dd73f8f4236a6f32
Pinto Moreno <alexpinto10@csu.fullerton.edu>
2025-04-13 13:16:21 -0700

    camera clmap

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,6 +1,9 @@
 extends Camera2D
 
-var speed: float = 200.0  # pixels per second
+var speed: float = 200.0  # pixels per secon
+# Define the bounds (in world space) the camera is allwowed to move
+@export var min_bound: Vector2 = Vector2(-36, -13)
+@export var max_bound: Vector2 = Vector2(520, 300)
 
 func _process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
@@ -11,3 +14,5 @@ func _process(delta: float) -> void:
 	if input_vector != Vector2.ZERO:
 		input_vector = input_vector.normalized()
 		position += input_vector * speed * delta
+	position.x = clamp(position.x, min_bound.x, max_bound.x)
+	position.y = clamp(position.y, min_bound.y, max_bound.y)
